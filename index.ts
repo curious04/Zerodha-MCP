2c8c82afff9a294f3a9c88b35f615266aa7519ea
Hrithik Singh <96012516+curious04@users.noreply.github.com>
2025-04-28 10:56:11 +0000

    mcp server tool demo

--- index.ts
+++ index.ts
@@ -1,3 +1,41 @@
 import { placeOrder } from "./trade";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import {z} from "zod";
 
-placeOrder("RELIANCE", 1, "BUY")
+// Create an MCP server
+const server = new McpServer({
+    name: "Demo",
+    version: "1.0.0"
+});
+
+// Add an addition tool
+server.tool("add two numbers",
+    {a: z.number(), b: z.number() },
+    async ({ a, b }) => ({
+    content: [{ type: "text", text: String(a + b) }]
+    })
+);
+
+server.tool("Buy a stock",
+    { stock: z.string(), qty: z.number() },
+        async ({stock, qty }) => {
+        placeOrder(stock, qty, "BUY");
+        return {
+            content: [{ type: "text", text: "Stock has been bought" }]
+    }
+}
+);
+
+server.tool("Sell a stock",
+    { stock: z.string(), qty: z.number() },
+        async ({stock, qty }) => {
+        placeOrder(stock, qty, "SELL");
+        return {
+            content: [{ type: "text", text: "Stock has been bought" }]
+    }
+}
+);
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
